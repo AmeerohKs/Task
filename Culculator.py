5c32d5e627ca2b1de543a5845a0ca0d353db7d21
Ameeroh Kasansakul <138428080+AmeerohKs@users.noreply.github.com>
2024-12-12 16:31:29 +0700

    Update Culculator.py

--- Culculator.py
+++ Culculator.py
@@ -1,33 +1,42 @@
 def calculator():
-    print("This is Culculator and we will start now!")
+    print("This is Calculator and we will start now!")
     while True:
         try:
             num1 = float(input("Enter the 1st number:> "))
-            operation = input("Select an operation [ 1.(+) 2.(-) 3.(*) 4.(/) ]:> ")
-            num2 = float(input("Enter the 2cd number:> "))
+            operation = input("Select an operation [ + , - , * , / ]:> ")
             
-            if operation == '1':
+            while True:
+                try:
+                    num2 = float(input("Enter the 2nd number:> "))
+                    if operation == '/' and num2 == 0:
+                        print("Error: Division by zero is not allowed. Please enter a non-zero number.")
+                        continue  # วนซ้ำเพื่อขอค่า num2 ใหม่
+                    break
+                except ValueError:
+                    print("Invalid input for the second number! Please enter a valid number.")
+            
+            if operation == '+':
                 result = num1 + num2
-            elif operation == '2':
+                symbol = "+"
+            elif operation == '-':
                 result = num1 - num2
-            elif operation == '3':
+                symbol = "-"
+            elif operation == '*':
                 result = num1 * num2
-            elif operation == '4':
-                if num2 != 0:
-                    result = num1 / num2
-                else:
-                    print("Error: Division by zero is not allowed.")
-                    continue
+                symbol = "*"
+            elif operation == '/':
+                result = num1 / num2
+                symbol = "/"
             else:
                 print("Invalid operation. Please try again.")
                 continue
-            
-            print(f"The result of {num1} {operation} {num2} = {result}")
-        
+
+            print(f"The result of {num1} {symbol} {num2} = {result}")
+
         except ValueError:
             print("Invalid input! Please enter numbers only.")
             continue
-        
+
         choice = input("Would you like to make another calculation? (yes/no): ").lower()
         if choice != 'yes':
             print("See ya next time!")
